a9b7137df7138d0ece5595cb24ebe4c75ec6a73f
Mabel890 <mabelvillada890@gmail.com>
2021-07-10 09:01:38 -0500

    Configuración de archivos

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "127.0.0.1";
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Iniciando el proyecto en el puerto: http://${host}:${port}`);
+});
