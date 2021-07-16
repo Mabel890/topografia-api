d15a1e2b1774109432fa74b59a3dcc5fe6094635
Mabel890 <mabelvillada890@gmail.com>
2021-07-15 19:38:20 -0500

    Instalación de la libreria cors

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use(cors({
+  origin: '*'
+}));
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "127.0.0.1";
