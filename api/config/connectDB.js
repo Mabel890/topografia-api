f74be75b6207dd60b07fb2b08516781686f262a2
Mabel890 <mabelvillada890@gmail.com>
2021-07-30 18:47:53 -0500

    Conexión a Heroku

--- api/config/connectDB.js
+++ api/config/connectDB.js
@@ -2,14 +2,22 @@ require("dotenv").config();
 
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  database: process.env.DATABASE,
-  username: "postgres",
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: 5432,
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+  }
+  }
+);
 
 const db = {};
 const Users = require("../model/users")(sequelize, Sequelize);
@@ -23,7 +31,13 @@ db.sequelize = sequelize;
 db.Users = Users;
 db.Roles = Roles;
 db.Points = require("../model/points")(sequelize, Sequelize);
-db.TopographicBooklet = require("../model/topographicBooklet")(sequelize, Sequelize);
-db.TypeOfTopographicSurvey = require("../model/typeOfTopographicSurvey")(sequelize, Sequelize);
+db.TopographicBooklet = require("../model/topographicBooklet")(
+  sequelize,
+  Sequelize
+);
+db.TypeOfTopographicSurvey = require("../model/typeOfTopographicSurvey")(
+  sequelize,
+  Sequelize
+);
 
 module.exports = db;
